0cb4c0953c8e764f789ab2ae78ba8254686bc3b3
Charles Yach <CharlesY712@gmail.com>
2018-08-22 14:09:25 -0600

    Adding /ideas/add
    Using handlebars to create video idea form

--- app.js
+++ app.js
@@ -37,6 +37,12 @@ app.get('/about', (req, res) => {
   res.render('about');
 });
 
+// Add Idea Form
+app.get('/ideas/add', (req, res) => {
+  res.render('ideas/add');
+});
+
+
 const port = 5000;
 
 app.listen(port, () => {
