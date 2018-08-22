dd8fd53a04010fbc0b877d1084a8d6ae46e61620
Charles Yach <CharlesY712@gmail.com>
2018-08-22 17:51:08 -0600

    Adding POST with errors
    Adding body parser for form. Adding handlebar loop for error alert. Pushing errors, title, and details with post idea.

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = express();
 
 // Map global promise
 mongoose.Promise = global.Promise;
+
 // Connect to mongoose
 mongoose.connect('mongodb://localhost/vidjot-dev', {
   useNewUrlParser: true
@@ -22,6 +24,10 @@ app.engine('handlebars', exphbs({
   defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 // Index Route
 app.get('/', (req, res) => {
@@ -40,6 +46,28 @@ app.get('/about', (req, res) => {
 // Add Idea Form
 app.get('/ideas/add', (req, res) => {
   res.render('ideas/add');
+});
+
+// Process Form
+app.post('/ideas', (req, res) => {
+  let errors = [];
+
+  if (!req.body.title) {
+    errors.push({text: 'Please add a title.'})
+  }
+  if (!req.body.details) {
+    errors.push({text: 'Please add details.'})
+  }
+
+  if (errors.length > 0) {
+    res.render('ideas/add', {
+      errors: errors,
+      title: req.body.title,
+      details: req.body.details
+    });
+  } else {
+    res.send('passed')
+  }
 });
 
 
